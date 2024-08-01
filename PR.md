35133071bee2120d4d699792da2aa1a7ef3d929e
joaorodriguesss <a100711@alunos.uminho.pt>
2024-08-01 19:59:16 +0100

    atualização

--- PR.md
+++ PR.md
@@ -1,3 +1,8 @@
 mongoimport --db loja --collection relogios_homem --file relogios_homem.json --jsonArray
 
 mongoimport --db loja --collection relogios_mulher --file relogios_mulher.json --jsonArray
+
+
+
+
+mongoimport --db loja --collection relogios --drop --file relogios.json --jsonArray
--- html-site/app.js
+++ html-site/app.js
@@ -1,3 +1,4 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -33,7 +34,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/relogios', relogioRouter);
 app.use('/joias', joiaRouter);
-app.use('/', produtoRouter);
+app.use('/produtos', produtoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
